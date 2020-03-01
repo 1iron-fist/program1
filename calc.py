ced0b8067f159e5acfba0a02287bac45a1113b50
Kaustubh <you@example.com>
2020-03-01 19:25:49 +0530

    my commit

--- calc.py
+++ calc.py
@@ -8,14 +8,3 @@ def divide(x,y):
     pass
 def square(x,y):
     pass
-git add -A
-make change in the calc.py
-def multiply(x,y):
-    return x*y
-git diff (show the changes made in the files)
-
-git status (show the modified state)
-
-git add -A (all the chnages in file)
-
-git commit -m “my commit”

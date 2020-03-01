a5dda545e979e1126a1bc3ff2e462b69ee5cf2d9
Kaustubh <you@example.com>
2020-03-01 18:18:05 +0530

    pass changed to return

---
+++ calc.py
@@ -0,0 +1,21 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    pass
+def multiply(x,y):
+    pass
+def divide(x,y):
+    pass
+def square(x,y):
+    pass
+git add -A
+make change in the calc.py
+def multiply(x,y):
+    return x*y
+git diff (show the changes made in the files)
+
+git status (show the modified state)
+
+git add -A (all the chnages in file)
+
+git commit -m “my commit”
